b62a049e5657c157b10064a59cf339513f896e61
JavierIMV <moyavalerio.ji@gmail.com>
2023-02-09 18:08:36 -0300

    Producto Class

---
+++ AppPoolMaui/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace AppPoolMaui.Models
+{
+    [Table("productos")]
+    public class Producto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(50), Unique]
+        public string Nombre { get; set; }
+        [MaxLength(50)]
+        public double Precio { get; set; }
+        [MaxLength(50)]
+        public int Cantidad { get; set; }
+    }
+}
